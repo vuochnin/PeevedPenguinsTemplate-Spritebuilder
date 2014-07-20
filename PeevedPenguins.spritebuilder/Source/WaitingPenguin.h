a9a867202ac55f7a9c9034260d4f501c4da34288
VuochNin <NewOwner@Jesse-Hinshaws-MacBook-Pro.local>
2014-07-20 14:09:49 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by VuochNin on 7/20/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end
