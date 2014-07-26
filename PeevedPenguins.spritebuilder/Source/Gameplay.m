7922f7e5df1d005f7fc1acad81608e1de350a596
VuochNin <NewOwner@Jesse-Hinshaws-MacBook-Pro.local>
2014-07-26 13:43:34 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -31,7 +31,7 @@
     [_levelNode addChild:level];
     
     // visualize physics bodies & joints
-    _physicsNode.debugDraw = TRUE;
+    _physicsNode.debugDraw = FALSE;
     
     // nothing shall collide with our invisible nodes
     _pullbackNode.physicsBody.collisionMask = @[];
